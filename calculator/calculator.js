1de081c48901fca6c70313da27813c35770457db
Weizus <marcley.colin@aol.com>
2024-06-26 02:06:27 -0400

    some minor work done to the calculator js file

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,17 +21,29 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  let values = {amount : 25000, years : 6, rate : 7}
+  let amountUI = document.querySelector("loan-amount");
+  amountUI.value = values.amount;
+  let yearsUI = document.querySelector("loan-years");
+  yearsUI.value = values.years;
+  let rateUI = document.querySelector("loan-rate");
+  rateUI.value = values.rate;
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  updateMonthly(calculateMonthlyPayment(getCurrentUIValues()));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let monthlyRate = (values.rate / 100) / 12.0;
+  let total_number_of_payments = values.years * 12
+  return ((values.amount * monthlyRate) / 
+  (1 - Math.pow((1 + monthlyRate), total_number_of_payments))).toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
