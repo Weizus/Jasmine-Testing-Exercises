c631629e7ebb9a1631a7d9e0e3f3453321f2b9da
Weizus <marcley.colin@aol.com>
2024-06-26 14:45:49 -0400

    got basic tests running

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,13 @@
+describe('rates, years, amounts', () => {
+	it('should calculate the monthly rate correctly', function () {
+  		expect(calculateMonthlyPayment({amount : 25000, years : 6, rate : 7}).toEqual(426.23);
+	}
+}));
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
-});
-
-
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
+describe('results', () => {
+	it("should return a result with 2 decimal places", () => {
+  		expect(calculateMonthlyPayment({amount : 25000, years : 6, rate : 7})).toBeCloseTo(426.0, 2);
+	}
+}));
 
 /// etc
