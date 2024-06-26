3717b01b5f37e6089d059dbeecf4608b72c457e6
Weizus <marcley.colin@aol.com>
2024-06-26 17:39:49 -0400

    tip pool part one done.

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -10,7 +10,7 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(Object.keys(allServers).length).toEqual(1);
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
-
+	
   afterEach(function() {
 	serverNameInput.value = '';
 	for (let key in allServers) {
@@ -22,4 +22,15 @@ describe("Servers test (with setup and tear-down)", function() {
 		currServerUI.remove();
 	}
   });
+
+	it('should check the dom for changes brought on by updateServerTable()', () => {
+  	// Trusting the beforeEach call
+		let serverTableUIChildren = document.querySelector('#serverTable').children;
+		for (key in serverTableUIChildren) {
+			if (serverTableUIChildren[key].localName == 'tbody') {
+				// Logic: if there are children nodes within the tbody that means html is being updated by the updateServerTable() method.
+				expect(serverTableUIChildren[key].hasChildNodes()).toBe(true); 
+			}
+  	}
+	});
 });
