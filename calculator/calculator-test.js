6f5330a6a2963e0555012dc41e3f7deb61d89335
Weizus <marcley.colin@aol.com>
2024-06-26 14:46:45 -0400

    old version of test file wasn't updated. Learned today I'd have to do the git add again if I make changes after the first add

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,13 +1,13 @@
 describe('rates, years, amounts', () => {
-	it('should calculate the monthly rate correctly', function () {
-  		expect(calculateMonthlyPayment({amount : 25000, years : 6, rate : 7}).toEqual(426.23);
-	}
-}));
+	it('should calculate the monthly rate correctly', () => {
+  		expect(+calculateMonthlyPayment({amount : 25000, years : 6, rate : 7})).toEqual(426.23);
+	})
+});
 
 describe('results', () => {
 	it("should return a result with 2 decimal places", () => {
-  		expect(calculateMonthlyPayment({amount : 25000, years : 6, rate : 7})).toBeCloseTo(426.0, 2);
-	}
-}));
+  		expect(+calculateMonthlyPayment({amount : 25000, years : 6, rate : 7})).toBeCloseTo(426.23, 2);
+	})
+});
 
 /// etc
